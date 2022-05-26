972629f510a431520e485f22ebf067949b67422b
andresanjaya2506@gmail.com <66811005+AndreSanjaya@users.noreply.github.com>
2022-05-26 09:22:52 +0800

    Create: DOM Traversal

---
+++ DOM Traversal/script.js
@@ -0,0 +1,19 @@
+// DOM Traversal
+const close = document.querySelectorAll('.close');
+// for(let i = 0; i < close.length; i++) {
+//     close[i].addEventListener('click', function(e) {
+//         // close[i].parentElement.style.display = 'none';
+//         e.target.parentElement.style.display = 'none';
+//     });
+// }
+
+close.forEach(function(el){
+    el.addEventListener('click', function(e) {
+        e.target.parentElement.style.display = 'none';
+    })
+})
+
+const nama = document.querySelector('.nama');
+console.log(nama.nextElementSibling.nextElementSibling);
+console.log(nama.nextElementSibling);
+console.log(nama.previousElementSibling);
